feb13ef899fbbce07c044c35b836f84564fca38b
Danielle Higgins <higdan4024@gmail.com>
2023-07-13 13:31:05 -0400

    simulate rock paper scissors game

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # odin-rock-paper-scissors
+This project is a rock, paper scissors game written in JavaScript. Right now it is using the browser console, but later I will come back to this project and add a Graphical User Interface so that it will be more interactive.
 
 ## Skills
+Skills I will have demonstrated once completed:
+
+- Basic HTML, same as listed in the [odin-recipes](https://github.com/Danielle-Higgins/odin-recipes) project
+- Basic CSS, same as listed in the [odin-recipes](https://github.com/Danielle-Higgins/odin-recipes) project along with
+    - The box model
+    - Flexbox
+- Basic JavaScript
 
 ## Thoughts
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// create variable to keep track of how many times player won
+let playerWins = 0
+
+// create variable to keep track of how many times computer won
+let computerWins = 0
+
 // make computer play
 function getComputerChoice() {
     // generate a random number between 1-3 and assign Rock, Paper, Scissors to them
@@ -17,6 +23,8 @@ function getComputerChoice() {
     }
 }
 
+//console.log(getComputerChoice())
+
 // plays a single round
 // use toLowerCase() to make case-insensitive
 function playRound(playerSelection, computerSelection) {
@@ -26,25 +34,59 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection.toLowerCase() === "rock") {  // else if player equals rock
         // if computer equals paper, return "Player Lost! Paper beats Rock"
         if (computerSelection.toLowerCase() === "paper") {
+            computerWins++
             return `Player chose ${playerSelection} and Computer chose ${computerSelection}. Player Lost! Paper covers Rock`
-        } else if (computerSelection.toLowerCase() === "scissors") { // otherwise reutrn "Player Won! Rock beats Scissors"
+        } else { // otherwise return "Player Won! Rock beats Scissors"
+            playerWins++
             return `Player chose ${playerSelection} and Computer chose ${computerSelection}. Player Won! Rock beats Scissors`
         }
     } else if (playerSelection.toLowerCase() === "paper") { // same as before
         if (computerSelection.toLowerCase() === "rock") {
+            playerWins++
             return `Player chose ${playerSelection} and Computer chose ${computerSelection}. Player Won! Paper covers Rock`
         } else {
+            computerWins++
             return `Player chose ${playerSelection} and Computer chose ${computerSelection}. Player Lost! Scissors cuts Paper`
         }
     } else if (playerSelection.toLowerCase() === "scissors") { // same as before
         if (computerSelection.toLowerCase() === "rock") {
+            computerWins++
             return `Player chose ${playerSelection} and Computer chose ${computerSelection}. Player Lost! Rock beats Scissors`
         } else {
+            playerWins++
             return `Player chose ${playerSelection} and Computer chose ${computerSelection}. Player Won! Scissors cuts Paper`
         }
     }
 }
 
-const playerSelection = "rock"
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
+// const playerSelection = "rock"
+// const computerSelection = getComputerChoice()
+// console.log(playRound(playerSelection, computerSelection))
+
+// plays 5 round game
+function game() {
+    // loop through the game 5 times
+    for(let i = 1; i <= 5; i++) {
+
+        let user = prompt("Chose Rock, Paper, or Scissors")
+
+        if (user === null) {
+            console.log(`Canceling Game!`)
+            return
+        }
+        // call the playRound function
+        console.log(playRound(user, getComputerChoice()))
+    }
+
+    console.log(`Player Score: ${playerWins} \t Computer Score: ${computerWins}`)
+
+    if (playerWins > computerWins) {
+        console.log(`Player Wins! Player has the Highest Score!`)
+    } else if (computerWins > playerWins) {
+        console.log(`Computer Wins! Computer has the Highest Score!`)
+    } else {
+        console.log(`No One Wins! It's a Tie!`)
+    }
+}
+
+game()
